b54a20844c3de8c619e9d435d10fe2a4eba63dfe
Aman <amankhanna224466@gmail.com>
2021-07-01 07:29:52 +0200

    handlebars added, routing fixed

--- src/index.js
+++ src/index.js
@@ -4,22 +4,26 @@ const path = require('path');
 const port = 3000;
 
 //BUILT-IN MIDDLEWARE
-console.log(path.join(__dirname, '../public'));     //for reaching the public directory
 
+//console.log(path.join(__dirname, '../public'));     //for reaching the public directory
 const staticPath = path.join(__dirname, '../public');
 app.use(express.static(staticPath));
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../public/views'));
+console.log(path.join(__dirname, '../public/views'));
+
 //ENDPOINTS
 app.get('/', (req, res) => {
-    res.status(200).send('index.html');
+    res.status(200).render('index.hbs');
 });
 
 app.get('/about', (req, res) => {
-    res.status(200).send('about.html');
+    res.status(200).render('about.hbs');
 });
 
 app.get('/blog', (req, res) => {
-    res.status(200).send('blog.html');
+    res.status(200).render('blog.html');
 });
 
 //SERVER LISTENING
